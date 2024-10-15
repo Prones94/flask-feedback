e5f856913101228b2e9b901f9da2bcb423524144
Ian <ronesp.85@gmail.com>
2024-10-15 14:19:50 -0700

    included templates

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, redirect, url_for, flash, session
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from models import db
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask_project"
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['SECRET_KEY'] = 'foo123'
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///flask_feedback"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'foo123'
+
+db.init_app(app)
 
 @app.route('/')
 def home():
-  return "Hello, World"
+  return render_template('home.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
